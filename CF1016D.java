70a51ae252ff30ba4136db9dfe767db3fbeb4453
Menil Dhameliya <menildhameliya1234@gmail.com>
2025-04-10 18:45:16 +0530

    Accpted code for CF1016D.java

--- CF1016D.java
+++ CF1016D.java
@@ -1,27 +1,65 @@
 import java.util.*;
 public class CF1016D {
 
-    public static long findElement(int x1,int x2, int y1, int y2, int targetx, int targety)
+    public static long findElement(long x,long y, long size)
     {
-        if(x2-x1==1)
+        if(size==2)
         {
-            if(targetx==x2 && targety==y2) return 2;
-            if(targetx==x1 && targety==y1) return 1;
-            if(targetx==x2 && targety==y1) return 3;
-            else return 4;
+            if(x==1 && y==1) return 1;
+            if(x==2 && y==1) return 3;
+            if(x==1 && y==2) return 4;
+            return 2;
         }
-        int x=(x2-x1)/2;
-        if(targetx>x)
+
+        if(x>size/2 && y>size/2) return (size/2)*(size/2)+findElement(x-size/2,y-size/2,size/2);
+        if(x>size/2) return 2*(size/2)*(size/2)+ findElement(x-size/2,y,size/2);
+        if(y>size/2) return 3*(size/2)*(size/2)+ findElement(x,y-size/2,size/2);
+        return findElement(x,y,size/2);
+    }
+
+    public static void findCoor(long size, long num, long[] coor)
+    {
+        if(size<=2)
         {
-            if(targety>x) return (long)Math.pow(2,2*x-2)+findElement(x,x2,x,y2,targetx-x,targety-x);
-            else return (long)Math.pow(2,2*x-1)+findElement(x,x2,y1,x,targetx-x,targety);
+            if(num==1)
+            {
+                coor[0]++; coor[1]++;
+            }
+            else if(num==2)
+            {
+                coor[0]+=2; coor[1]+=2;
+            }
+            else if(num==3)
+            {
+                coor[0]+=2; coor[1]++;
+            }
+            else
+            {
+                coor[0]++; coor[1]+=2;
+            }
+            return;
         }
-        else
+
+        if(num>3*(size/2*size/2))
         {
-            if(targety>x) return (long)(3*Math.pow(2,2*x-2)+(findElement(x1,x,x,y2,targetx,targety-x)));
-            else return findElement(x1,x,y1,x,targetx,targety);
+            coor[1]+=size/2;
+            findCoor(size/2,num-3*(size/2*size/2),coor);
+        }
+        else if(num>2*(size/2*size/2))
+        {
+            coor[0]+=size/2;
+            findCoor(size/2, num-2*(size/2*size/2),coor);
+        }
+        else if(num>(size/2*size/2))
+        {
+            coor[0]+=size/2; coor[1]+=size/2;
+            findCoor(size/2,num-(size/2*size/2),coor);
+        }
+        else {
+            findCoor(size/2,num,coor);
         }
     }
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
@@ -31,9 +69,20 @@ public class CF1016D {
             int q=sc.nextInt();
             while(0<q--)
             {
-                int targetx=sc.nextInt();
-                int targety=sc.nextInt();
-                System.out.println(findElement(0,(int)Math.pow(2,n),0,(int)Math.pow(2,n),targetx,targety));
+                String s=sc.next();
+                if(s.charAt(0)=='-')
+                {
+                    long targetx=sc.nextLong();
+                    long targety=sc.nextLong();
+                    System.out.println(findElement(targetx, targety,(int)Math.pow(2,n)));
+                }
+                else
+                {
+                    long target=sc.nextLong();
+                    long[] arr=new long[2]; arr[0]=0; arr[1]=0;
+                    findCoor((long)Math.pow(2,n),target,arr);
+                    System.out.println(arr[0]+" "+arr[1]+" ");
+                }
             }
         }
     }
